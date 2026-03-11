bca40ac58796393f5f670e731f496493132e8119
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-03-11 11:43:57 +0530

    Added insert method
    Added insert value method inside a BST while preserving its balanced nature as well

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -14,11 +14,11 @@ class Tree {
 
     buildTree(array, start, end) {
 
-        if(start > end) return null;
+        if (start > end) return null;
 
         const mid = Math.floor(start + (end - start) / 2);
-             
-        const root = new Node(array[mid]);            
+
+        const root = new Node(array[mid]);
 
         root.left = this.buildTree(array, start, mid - 1);
         root.right = this.buildTree(array, mid + 1, end);
@@ -29,9 +29,9 @@ class Tree {
     includes(value) {
         let current = this.root;
 
-        while(current) {
-            if(value === current.data) {
-              return true;
+        while (current) {
+            if (value === current.data) {
+                return true;
             } else if (value < current.data) {
                 current = current.left;
             } else {
@@ -41,22 +41,49 @@ class Tree {
 
         return false;
     }
+
+    insert(value) {
+        let current = this.root;
+
+        while (true) {
+            if (value === current.data) {
+                return;
+            }
+
+            if (value < current.data) {
+                if (current.left === null) {
+                    current.left = new Node(value);
+                    return;
+                } else {
+                    current = current.left;
+                }
+            } else {
+                if (current.right === null) {
+                    current.right = new Node(value);
+                    return;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+
+    }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
-  }
+    if (node === null) {
+        return;
+    }
 
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
 
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
 
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
 };
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5]);
