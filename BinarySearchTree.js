09db3ff109938a349187d8a1cd3bebfe5f6898d8
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-03-11 10:16:12 +0530

    Added prettyPrint functionality
    Added prettyPrint functionality to test out my BST visually

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -27,3 +27,22 @@ class Tree {
     }
 }
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5]);
+
+prettyPrint(tree.root);
